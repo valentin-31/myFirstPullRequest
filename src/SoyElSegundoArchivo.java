a761a79486d72402845e53d25951da334afce896
Valentin <valen.manestar@gmail.com>
2026-08-24 21:51:03 -0300

    Invertir el orden de la cuenta del segundo archivo

--- src/SoyElSegundoArchivo.java
+++ src/SoyElSegundoArchivo.java
@@ -1,10 +1,10 @@
 public class SoyElSegundoArchivo {
     public static void main(String[] args) {
 
-        System.out.println("Soy el segundo archivo, solo cuento desde el 1 al 10 :(");
+        System.out.println("Soy el segundo archivo, ahora cuento desde 10 al 1 (?");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + 1);
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
         }
     }
 }
