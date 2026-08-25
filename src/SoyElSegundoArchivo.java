68ab957da8708f2dfb098af1716e866c9d4c0192
Valentin <valen.manestar@gmail.com>
2026-08-24 21:48:44 -0300

    Crear el segundo archivo

---
+++ src/SoyElSegundoArchivo.java
@@ -0,0 +1,10 @@
+public class SoyElSegundoArchivo {
+    public static void main(String[] args) {
+
+        System.out.println("Soy el segundo archivo, solo cuento desde el 1 al 10 :(");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + 1);
+        }
+    }
+}
