69e8bee687b743083084d613f1f89e27905f11e7
Valentin <valen.manestar@gmail.com>
2026-08-24 21:41:17 -0300

    Preguntar el nombre del user para saludarlo

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        System.out.print("Cual es tu nombre: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nombre = scanner.nextLine();
+
+        System.out.println("Hola " + nombre + "!");
+
     }
 }
